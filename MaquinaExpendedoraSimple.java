5ace5b493c2a14d04564c078bb3b115fe47612da
Ivxn <gonzalez.martino.ivan@gmail.com>
2021-10-29 13:52:51 +0200

    02-2 constructores y maquina nueva con destino fijo

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -23,6 +23,19 @@ public class MaquinaExpendedoraSimple {
         estacionOrigen = "León";
         estacionDestino = "Madrid";
     }
+    
+    /**
+     * Crea una maquina expendedora de billetes de tren con el 
+     * precio del billete y el origen y destino dados. Se asume que el precio
+     * del billete que se recibe es mayor que 0.
+     */
+    public MaquinaExpendedoraSimple(int precioBillete,String estacionDestino) {
+        precioBillete = 0;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = "León";
+        estacionDestino = "destino";
+    }
 
     /**
      * Devuelve el precio del billete
